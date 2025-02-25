e3302ff91e9436b4fdd3abe9478026bf60e3e75c
JoshsDesk <Joshsdesk@gmail.com>
2025-02-24 19:02:43 -0700

    Add files via upload

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,76 +1,77 @@
-import ReactDOM from "react-dom/client";
-import "bootstrap/dist/css/bootstrap.min.css";
-import { createBrowserRouter, RouterProvider, Navigate } from "react-router-dom";
-import "./index.css";
+import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import './index.css';
 
-import App from "./App.tsx";
-import ErrorPage from "./pages/ErrorPage.tsx";
-import Home from "./pages/Home.tsx";
-import Login from "./pages/Login.tsx";
-import Inventory from "./pages/Inventory.tsx";
-import Reports from "./pages/Reports.tsx";
-import TimeClock from "./pages/TimeClock.tsx";
-import PayRoll from "./pages/PayRoll.tsx"; 
-import Customers from "./pages/Customers.tsx"; 
-import Suppliers from "./pages/Suppliers.tsx"; 
-import Services from "./pages/Services.tsx";
-import Billing from "./pages/Billing.tsx";
+import App from './App.tsx';
+
+import ErrorPage from './pages/ErrorPage.tsx';
+import Home from './pages/Home.tsx';
+import Login from './pages/Login.tsx';
+import Inventory from './pages/Inventory.tsx';
+import Users from './pages/Employee.tsx'
+import TimeClock from './pages/TimeClock.tsx';
+import PayRoll from './pages/PayRoll.tsx';
+import Customers from './pages/Customers.tsx';
+import Suppliers from './pages/Suppliers.tsx';
+import Services from './pages/Services.tsx';
+import Reports from './pages/Reports.tsx';
+import Settings from './pages/Settings.tsx';
 
 const router = createBrowserRouter([
   {
-    path: "/",
-    element: <Navigate to="/login" replace />,
-    errorElement: <ErrorPage />,
-  },
-  {
-    path: "/login",
-    element: <Home />,
-  },
-  {
-    path: "/app",
+    path: '/',
     element: <App />,
+    errorElement: <ErrorPage />,
     children: [
       {
         index: true,
+        element: <Login />,
+      },
+      {
+        path: '/home',
         element: <Home />,
       },
       {
-        path: "inventory",
-        element: <Inventory />,
+        path: '/timeclock',
+        element: <TimeClock />
       },
       {
-        path: "reports",
-        element: <Reports />,
+        path: '/payroll',
+        element: <PayRoll />
       },
       {
-        path: "time-clock",
-        element: <TimeClock />,
+        path: '/customers',
+        element: <Customers />
       },
       {
-        path: "payroll",
-        element: <PayRoll />,
+        path: '/suppliers',
+        element: <Suppliers />
       },
       {
-        path: "customers",
-        element: <Customers />,
+        path: '/services',
+        element: <Services />
       },
       {
-        path: "suppliers",
-        element: <Suppliers />,
+        path: '/inventory',
+        element: <Inventory />
       },
       {
-        path: "services",
-        element: <Services />,
+        path: '/reports',
+        element: <Reports />
       },
       {
-        path: "billing",
-        element: <Billing />,
+        path: '/settings',
+        element: <Settings />
       },
+      {
+        path: "/users",
+        element: <Users />
+      }
     ],
   },
 ]);
 
-const rootElement = document.getElementById("root");
+const rootElement = document.getElementById('root');
 if (rootElement) {
   ReactDOM.createRoot(rootElement).render(<RouterProvider router={router} />);
 }
