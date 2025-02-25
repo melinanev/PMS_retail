6e25090ff2dc408caafc342aa990ace086824124
JoshsDesk <Joshsdesk@gmail.com>
2025-02-24 18:40:41 -0700

    Update Reports.tsx

--- pages/Reports.tsx
+++ pages/Reports.tsx
@@ -10,7 +10,7 @@ const Reports = () => {
         <img className="soon-png" src={SoonCome} alt="Coming soon placeholder" />
       </div>
       <div className="soon-text">
-        <h2>Hang tight! The Time Clock feature is coming soon!</h2>
+        <h2>Hang tight! This feature is coming soon!</h2>
         <button className="soon-button" onClick={() => navigate("/home")}>Go Back Home</button>
       </div>
       
